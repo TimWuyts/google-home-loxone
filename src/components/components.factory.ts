bb6fbd98c56f03cc0e5ba4f99694b9a18befea75
Tim Wuyts <tim@digiti.be>
2019-02-02 20:15:27 +0100

    remove pool component

--- src/components/components.factory.ts
+++ src/components/components.factory.ts
@@ -6,7 +6,6 @@ import {CustomOnOff} from './custom-on-off';
 import {DoorComponent} from './door';
 import {JalousieComponent} from './jalousie';
 import {LightComponent} from './light';
-import {PoolComponent} from './pool';
 import {TemperatureComponent} from './temperature';
 
 export class ComponentsFactory {
@@ -22,9 +21,6 @@ export class ComponentsFactory {
             break;
           case 'Door':
             component = new DoorComponent(rawComponent, loxoneRequest, statesEvents);
-            break;
-          case 'Pool':
-            component = new PoolComponent(rawComponent, loxoneRequest, statesEvents);
             break;
           case 'Temperature':
             component = new TemperatureComponent(rawComponent, loxoneRequest, statesEvents);
